69fff4606d008f36e04b030de2d8313cec63006d
hazarduman0 <93342169+hazarduman0@users.noreply.github.com>
2022-07-03 13:44:39 +0300

    Update database_quiz.dart

---
+++ lib/db/database_quiz.dart
@@ -0,0 +1,7 @@
+import 'package:memorize/db/database.dart';
+import 'package:memorize/model/quiz.dart';
+
+class QuizOperations {
+  DatabaseRepository dbRepository = DatabaseRepository.instance;
+
+}
