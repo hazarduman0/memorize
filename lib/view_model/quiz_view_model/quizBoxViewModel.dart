73f6907871cd3689c38ee5e90d1e5c100418080c
hazarduman0 <93342169+hazarduman0@users.noreply.github.com>
2022-07-17 16:02:30 +0300

    Update quizBoxViewModel.dart

--- lib/view_model/quiz_view_model/quizBoxViewModel.dart
+++ lib/view_model/quiz_view_model/quizBoxViewModel.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:memorize/constants/appTextStyles.dart';
 import 'package:memorize/constants/projectKeys.dart';
+import 'package:memorize/db/database_quiz.dart';
+import 'package:memorize/db/database_word.dart';
 
 abstract class QuizBoxViewModel<T extends StatefulWidget> extends State<T> {
   AppTextStyles textStyles = AppTextStyles();
@@ -8,10 +10,39 @@ abstract class QuizBoxViewModel<T extends StatefulWidget> extends State<T> {
   ProjectKeys keys = ProjectKeys();
 
   bool openedCard = false;
+  bool haveThreeQuiz = false;
+  bool haveTenWord = false;
+  bool showWordInformation = false;
+
+  WordOperations wordOperations = WordOperations();
+  QuizOperations quizOperations = QuizOperations();
 
   lastExamTextButtonFunc() {
     setState(() {
       openedCard = !openedCard;
+    });
+  }
+
+  setHaveThreeQuiz() {
+    setState(() {
+      haveThreeQuiz = true;
+    });
+  }
+
+  setHaveTenWord() {
+    setState(() {
+      haveTenWord = true;
+    });
+  }
+
+  void notEnoughWordFunc() async {
+    setState(() {
+      showWordInformation = true;
+    });
+    await Future.delayed(const Duration(seconds: 5));
+
+    setState(() {
+      showWordInformation = false;
     });
   }
 }
