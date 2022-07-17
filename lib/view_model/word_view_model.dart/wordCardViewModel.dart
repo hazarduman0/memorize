182209607d332def5ce3cc1946da19cc4f689aed
hazarduman0 <93342169+hazarduman0@users.noreply.github.com>
2022-07-17 18:02:09 +0300

    Update wordCardViewModel.dart

--- lib/view_model/word_view_model.dart/wordCardViewModel.dart
+++ lib/view_model/word_view_model.dart/wordCardViewModel.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:memorize/constants/appTextStyles.dart';
 import 'package:memorize/db/database_meaning.dart';
+import 'package:memorize/model/meaning.dart';
 
 abstract class WordCardViewModel<T extends StatefulWidget> extends State<T> {
   AppTextStyles textStyles = AppTextStyles();
   bool cardBool = false;
   MeaningOperations meaningOperations = MeaningOperations();
+  List<Meaning> meanings = [];
 
   showLessButtonFunc() {
     setState(() {
@@ -17,5 +19,14 @@ abstract class WordCardViewModel<T extends StatefulWidget> extends State<T> {
     setState(() {
       cardBool = true;
     });
+  }
+
+  initMeanings(int? wordId) async{
+    List<Meaning> _meanings = await meaningOperations.getWordMeanings(wordId);
+    if(mounted){
+      setState(()  {
+      meanings = _meanings;
+    });
+    }
   }
 }
