3a7c9dc58d42b8cbc9d51abcbee23d2833a500d0
Hazar Duman <93342169+hazarduman0@users.noreply.github.com>
2022-07-24 19:30:35 +0300

    Update wordCardViewModel.dart

--- lib/view_model/word_view_model.dart/wordCardViewModel.dart
+++ lib/view_model/word_view_model.dart/wordCardViewModel.dart
@@ -1,32 +1,32 @@
-import 'package:flutter/material.dart';
-import 'package:memorize/constants/appTextStyles.dart';
-import 'package:memorize/db/database_meaning.dart';
-import 'package:memorize/model/meaning.dart';
+// import 'package:flutter/material.dart';
+// import 'package:memorize/constants/appTextStyles.dart';
+// import 'package:memorize/db/database_meaning.dart';
+// import 'package:memorize/model/meaning.dart';
 
-abstract class WordCardViewModel<T extends StatefulWidget> extends State<T> {
-  AppTextStyles textStyles = AppTextStyles();
-  bool cardBool = false;
-  MeaningOperations meaningOperations = MeaningOperations();
-  List<Meaning> meanings = [];
+// abstract class WordCardViewModel<T extends StatefulWidget> extends State<T> {
+//   AppTextStyles textStyles = AppTextStyles();
+//   bool cardBool = false;
+//   MeaningOperations meaningOperations = MeaningOperations();
+//   List<Meaning> meanings = [];
 
-  showLessButtonFunc() {
-    setState(() {
-      cardBool = false;
-    });
-  }
+//   showLessButtonFunc() {
+//     setState(() {
+//       cardBool = false;
+//     });
+//   }
 
-  showMoreButtonFunc() {
-    setState(() {
-      cardBool = true;
-    });
-  }
+//   showMoreButtonFunc() {
+//     setState(() {
+//       cardBool = true;
+//     });
+//   }
 
-  initMeanings(int? wordId) async{
-    List<Meaning> _meanings = await meaningOperations.getWordMeanings(wordId);
-    if(mounted){
-      setState(()  {
-      meanings = _meanings;
-    });
-    }
-  }
-}
+//   initMeanings(int? wordId) async{
+//     List<Meaning> _meanings = await meaningOperations.getWordMeanings(wordId);
+//     if(mounted){
+//       setState(()  {
+//       meanings = _meanings;
+//     });
+//     }
+//   }
+// }
